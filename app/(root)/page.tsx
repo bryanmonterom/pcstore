e096516925a48250b47c25a81e6cc9d6f43743d9
Bryan <bryanmonterom@gmail.com>
2025-01-20 18:51:10 -0500

    Connect ProductList to the DB
    Adding a parsing function for objects comming from PRISMA
    Adding a new constant to Limit product retrieval from the DB

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
-import sampleData from "@/db/sample-data";
+import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
 import ProductList from '../../components/ui/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product-actions';
 
-const HomePage = () => {
-  return ( <><ProductList data={sampleData.products} title="Newest arrivals"/></> );
-}
- 
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList data={latestProducts} title="Newest arrivals" limit={LATEST_PRODUCTS_LIMIT}/>
+    </>
+  );
+};
+
 export default HomePage;
