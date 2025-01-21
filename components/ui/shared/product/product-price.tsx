0910bd6c8e33655c46e212503cccde073cac3621
Bryan <bryanmonterom@gmail.com>
2025-01-20 20:28:07 -0500

    Install badge component and create product detail page

--- components/ui/shared/product/product-price.tsx
+++ components/ui/shared/product/product-price.tsx
@@ -1,14 +1,21 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-const ProductPrice  = ({value, className}:{value: number; className?: string}) => {
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  const stringValue = value.toFixed(2);
+  const [intValue, floatValue] = stringValue.split('.');
+  return (
+    <p className={cn('text-2xl', className)}>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+      </p>
+  );
+};
 
-    const stringValue = value.toFixed(2);
-    const [intValue, floatValue] = stringValue.split('.')
-    return ( <p className= {cn('text-2xl', className)}>
-        <span className="text-xs align-supper">${intValue}</span>
-        <span className="text-xs align-supper">.{floatValue}</span>
-
-    </p> );
-}
- 
 export default ProductPrice;
