a8ba14be3abb75febdc419a54aa96a8742f23442
Bryan <bryanmonterom@gmail.com>
2025-01-21 22:05:10 -0500

    Configuring seed for the user data

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,19 @@
+import {hashSync} from 'bcrypt-ts-edge'
 const sampleData = {
+  users:[
+    {
+      name:'Bryan',
+      email:'admin@example.com',
+      password:hashSync('123456',10),
+      role:'admin'
+    },
+    {
+      name:'Abel',
+      email:'user@example.com',
+      password:hashSync('123456',10),
+      role:'user'
+    }
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
