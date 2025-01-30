1255ac2e019ea8371fb024fc8c88360dfb8d2d0b
Bryan <bryanmonterom@gmail.com>
2025-01-30 15:17:59 -0500

    Improve error handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
+import { formatError } from '../utils';
 
 //sign in the user with credentials
 export default async function signInWithCredentials(
@@ -34,40 +35,39 @@ export async function signOutUser() {
   await signOut();
 }
 
-export async function signUpUser(prevState: unknown, formData:FormData){
-
+export async function signUpUser(prevState: unknown, formData: FormData) {
   try {
     const user = signUpFormShema.parse({
-      name:formData.get('name'),
-      email:formData.get('email'),
-      password:formData.get('password'),
-      confirmPassword:formData.get('confirmPassword'),
-
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
     });
 
-    const plainPassword = user.password
-    user.password = hashSync(user.password, 10)
+    const plainPassword = user.password;
+    user.password = hashSync(user.password, 10);
     await prisma.user.create({
-      data:{
-        name:user.name,
-        email:user.email,
-        password:user.password
-      }
-    })
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
 
-    await signIn('credentials',{
-      email:user.email,
-      password:plainPassword
-    })
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
 
-    return {success:true, message:'User registered succesfully'}
-
+    return { success: true, message: 'User registered succesfully' };
   } catch (error) {
     if (isRedirectError(error)) {
       throw error;
     }
-    console.log(error)
-    return { success: false, message: 'Something failed to create the new user' };
-
+   formatError(error);
+    return {
+      success: false,
+      message:  formatError(error)
+    };
   }
 }
