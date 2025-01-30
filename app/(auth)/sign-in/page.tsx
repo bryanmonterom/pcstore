cc8a68c4554d7cc7c36b8198231fcede342013d9
Bryan <bryanmonterom@gmail.com>
2025-01-29 21:51:10 -0500

    Adding new UI components and sign in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from '@/lib/constants';
 import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import CredentialsSignInForm from './credentials-signin-form';
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -25,7 +26,7 @@ const SignInPage = () => {
           <CardDescription className='text-center'>Sign in to your account</CardDescription>
         </CardHeader>
         <CardContent className='space-y-4'>
-            { }
+            <CredentialsSignInForm></CredentialsSignInForm>
         </CardContent>
       </Card>
     </div>
