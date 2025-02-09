2113b0cc28ed09b4e72b7c638c10b81b3c9c0624
Bryan <bryanmonterom@gmail.com>
2025-02-09 16:49:58 -0500

    Adding unit tests for createOrder and capture payment

--- tests/paypal.test.ts
+++ tests/paypal.test.ts
@@ -1,10 +1,36 @@
-import { generateAccessToken } from "../lib/paypal";
+import { generateAccessToken, paypal } from "../lib/paypal";
 
 //Test to generate access token from paypal
 test('Generates a token from paypal', async () =>{
     const tokenResponse = await generateAccessToken();
-    
+    console.log(tokenResponse)
+
     expect(typeof tokenResponse).toBe('string')
     expect(tokenResponse.length).toBeGreaterThan(0)
 
+})
+
+//Test to create an order
+test('Create paypal Order', async () =>{
+    const price = 10.0
+    const response = await paypal.createOrder(price);
+    console.log(response)
+
+    expect(response).toHaveProperty('id');
+    expect(response).toHaveProperty('status');
+    expect(response.status).toBe('CREATED');
+})
+
+test('Simulate capturing a payment from an order', async () =>{
+    const price = 10.0
+    const orderId = '100';
+
+    const mockCapturePayment = jest.spyOn(paypal ,'capturePayment').mockResolvedValue({status:'COMPLETED'})
+
+    const response = await paypal.capturePayment(orderId);
+    console.log(response)
+
+    expect(response).toHaveProperty('status');
+    expect(response.status).toBe('COMPLETED');
+    mockCapturePayment.mockRestore()
 })
