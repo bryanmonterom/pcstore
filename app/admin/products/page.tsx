5087bf203186e0ea791ea7c749143ae7eb0ebdd4
Bryan <bryanmonterom@gmail.com>
2025-02-20 11:48:37 -0500

    Add getAllProducts action

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,23 @@
+import { getAllProducts } from "@/lib/actions/product.actions";
+
+const AdminProductPage = async (props:{
+    searchParams: Promise<{page: string; query:string; category:string}>
+}) => {
+
+    const searchParams = await props.searchParams;
+    const page = Number(searchParams.page) || 1;
+    const searchText = searchParams.query || '';
+    const category = searchParams.category || '';
+
+    const products = await getAllProducts({query:searchText,page , category})
+
+    
+
+    return (<div className="space-y-2">
+        <div className="flex-between">
+            <h1 className="h2-bold"></h1>
+        </div>
+    </div> );
+}
+ 
+export default AdminProductPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 'use server';
 import { prisma } from '../../db/prisma';
 import { convertToPlainObject } from '../utils';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 
 //Get latest products
 export async function getLatestProducts() {
@@ -18,4 +18,28 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+export async function getAllProducts({
+  query,
+  page,
+  limit = PAGE_SIZE,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
