eda14e932cf554aee16e9f6a7cf1faabc2f282a4
Bryan <bryanmonterom@gmail.com>
2025-02-20 21:35:02 -0500

    Adding the update user form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -32,22 +32,29 @@ export const shippingAddressDefaultValues: ShippingAddress = {
   country: '',
 };
 
-export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(',') : ['Paypal', 'Stripe', 'CashOnDelivery'];
-export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || "Paypal"	;
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(',')
+  : ['Paypal', 'Stripe', 'CashOnDelivery'];
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || 'Paypal';
 
-export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 5
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 5;
 
 export const productDefaultValues = {
-  name:'',
-  slug:'',
-  category:'',
-  images:[],
-  brand:'',
-  description:'',
-  price:'0',
-  stock:0,
-  rating:'0',
-  numReviews:'0',
-  isFeatured:false,
-  banner:null
-}
+  name: '',
+  slug: '',
+  category: '',
+  images: [],
+  brand: '',
+  description: '',
+  price: '0',
+  stock: 0,
+  rating: '0',
+  numReviews: '0',
+  isFeatured: false,
+  banner: null,
+};
+
+export const USER_ROLES = process.env.USER_ROLES
+  ? process.env.USER_ROLES.split(', ')
+  : ['admin', 'user'];
